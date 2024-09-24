c31e4fe7c63de0b3f5b353fd9b36e85004e4ef48
AramisB <brendachem7@gmail.com>
2024-09-24 15:40:03 +0300

    using promisify with async

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -3,6 +3,7 @@ import { promisify } from "util";
 
 const client = redis();
 const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
 client.on("error", (error) => {
     console.log("Redis client not connected to the server: " + error);
@@ -12,17 +13,36 @@ client.on("connect", () => {
     console.log("Redis client connected to the server");
 });
 
-client.connect()
-
-function setNewSchool(schoolName, value){
-    client.set(schoolName, value, redis.print);    
+async function connectToRedis() {
+    try {
+        await client.connect();
+    } catch (error) {
+        console.error("Could not connect to Redis:", error);
+    }
 }
 
-async function displaySchoolValue(schoolName){
-    const result = await client.get(schoolName);
-    console.log(result);
-};
+async function setNewSchool(schoolName, value) {
+    try {
+        await setAsync(schoolName, value);
+    } catch (error) {
+        console.error("Error setting school value: ", error);
+    }
+}
 
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+async function displaySchoolValue(schoolName) {
+    try {
+        const result = await getAsync(schoolName);
+        console.log(result);
+    } catch (error) {
+        console.error("Error getting school value: ", error);
+    }
+}
+
+async function run() {
+    await connectToRedis();
+    await displaySchoolValue('Holberton');
+    await setNewSchool('HolbertonSanFrancisco', '100');
+    await displaySchoolValue('HolbertonSanFrancisco');
+}
+
+run();
